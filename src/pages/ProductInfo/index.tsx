30bee287488e3701251efaa8b8d749b0cf2a6adf
Yijun Li <yijun.li@shopee.com>
2024-09-25 11:03:21 +0800

    ProductInfo 初始页面

--- src/pages/ProductInfo/index.tsx
+++ src/pages/ProductInfo/index.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Pressable, ScrollView, StyleSheet, TextInput, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
 
 function ProductInfo() {
   return (
-    <View>
-      <Text>ProductInfo</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.searchContainer}>
+        <Pressable style={styles.searchBox}>
+          <AntDesign name="search1" size={24} style={styles.searchIcon} />
+          <TextInput placeholder="Search Amazon.in" />
+        </Pressable>
+        <Feather name="mic" size={24} />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flex: 1,
+    backgroundColor: '#00CED1',
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchBox: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    gap: 10,
+    borderRadius: 3,
+    height: 38,
+    backgroundColor: 'white',
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+});
 
 export default ProductInfo;
